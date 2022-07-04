350a5d84379a93f98ad3e819c1662eba6c9d0984
백만기 <manki.baek@seculayer.com>
2022-07-04 17:51:30 +0900

    seq_term 고정 (GPRM 추가)

--- hprs/recommender/RandomRecommender.py
+++ hprs/recommender/RandomRecommender.py
@@ -47,6 +47,8 @@ class RandomRecommender(object):
                     elif param.get("param_type") == "1":
                         if param_nm == "n_neighbors":
                             param_dict[param_nm] = random.randint(2, 3)
+                        elif param_nm == "seq_term":
+                            param_dict[param_nm] = 60
                         else:
                             param_dict[param_nm] = random.randint(1, 16)
                     elif param.get("param_type") == "2":
